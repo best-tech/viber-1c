e123fba24ff5de16aa6122041314523ac34ab415
Денис Харченко <kharchenko_di@art-vizing.ru>
2017-02-13 10:50:29 +0300

    re

--- web/index.php
+++ web/index.php
@@ -1,5 +1,41 @@
 <?php
-require('../vendor/autoload.php');
+//require('../vendor/autoload.php');
 
-echo 'yes '.getenv('TIMES');
+header('Content-Type: application/json');
+
+$REQUEST_METHOD = $_SERVER['REQUEST_METHOD'];
+
+if ($REQUEST_METHOD=='POST')
+{
+$text = implode("", file('php://input'));
+}
+else
+{
+    if (isset($_GET['q']))
+    $text = $_GET['q'];
+    else  $text ='{"no data":"false"}';
+}
+
+echo $text;
+//echo $text;
+// json_decode($text);
+//var_export($text);
+
+return;
+	// отправляем запрос
+	$responce = file_get_contents('http://4098.ru/viber-1c', 
+		false, 
+		stream_context_create(
+			array(
+				'http' => array(
+					'method' => 'POST',
+					'header' => 'Content-Type: application/json; ',
+					'content' => $text
+				)
+			)
+		)
+	);
+    
+    var_export($responce);
+
 ?>

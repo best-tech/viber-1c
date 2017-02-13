01a56343fb80120a57648d86e6e338dac15ec1c4
Денис Харченко <kharchenko_di@art-vizing.ru>
2017-02-13 17:10:19 +0300

    f

--- web/index.php
+++ web/index.php
@@ -5,30 +5,30 @@ header('Content-Type: application/json');
 
 $REQUEST_METHOD = $_SERVER['REQUEST_METHOD'];
 
+$queryAddr = '';
+
 if ($REQUEST_METHOD=='POST')
 {
-$text = implode("", file('php://input'));
+	$text = implode("", file('php://input'));
 }
 else
 {
-    if (isset($_GET['q']))
-    $text = $_GET['q'];
-    else  {$text ='{"no data":"false"}'; echo $text; return;}
+    if (isset($_GET['q'])) 
+		$queryAddr = '?q='.$_GET['q'];
 }
 
 
-//echo $text;
-// json_decode($text);
-//var_export($text);
+
+
 
 //return;
 	// отправляем запрос
-	$responce = file_get_contents('http://4098.ru/viber-1c', 
+	$responce = file_get_contents('http://4098.ru/viber-1c'.$queryAddr, 
 		false, 
 		stream_context_create(
 			array(
 				'http' => array(
-					'method' => 'POST',
+					'method' => $REQUEST_METHOD,
 					'header' => 'Content-Type: application/json; ',
 					'content' => $text
 				)

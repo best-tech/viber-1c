9a6a4d3f9b838457a802ace52ab8c0fc2c7bd62f
Денис Харченко <kharchenko_di@art-vizing.ru>
2017-02-15 12:38:10 +0300

    f

--- web/index.php
+++ web/index.php
@@ -3,82 +3,96 @@ require('../vendor/autoload.php');
 
 //header('Content-Type: application/json');
 
-$connectionString =  getenv('MONGODB_URI');
+$REQUEST_METHOD = $_SERVER['REQUEST_METHOD'];
 
-if (!$connectionString) $connectionString ="mongodb://heroku_7jhm3vw7:usl2213vp3pdhlaj3h0a1jo11m@ds153179.mlab.com:53179/heroku_7jhm3vw7";
+$limit = 100;
 
-$arr = array_reverse(explode('/', $connectionString));
+if (isset($_GET['limit'])&&is_int((int) $_GET['limit'])) $limit=(int) $_GET['limit'];
 
-$dbName = $arr[0];
 
-if (!$dbName) {echo 'no db name'; return;};
+if ($REQUEST_METHOD=='POST')
+{
+	$text = (string) implode("", file('php://input'));
+	insertOne($text);
+	echo $responce;
+}
+else
+{
+	
+    if (isset($_GET['q'])) 
+	{
+		$text = $_GET['q'];
+		$eRR = insertOne($text);
 
-//$connectionString = str_last_replace("/".$dbName,"",$connectionString);
+		if($eRR) $text = $eRR;
+		
+	}	
+	else
+	{
 
-if (!$connectionString) {echo 'no connection string'; return;};
+		$text = ReadData($limit);		
 
-$client = new MongoDB\Client($connectionString);
+	}
+	echo $text;
+}
 
-$DataBase = $client->$dbName;
-$collection = $DataBase->incoming;
 
-$post = array(
-         'title'     => 'Что такое MongoDB',
-         'content'   => 'MongoDB это высокопроизводительная документо-ориентированная база данных...'
+function ReadData($limit)
+{
+
+	$collection = getConnectionDB();
+	if (!$collection) return;
+
+}
+function insertOne($text)
+{
+	try {
+		$jsObject = json_decode($text);
+		
+	} catch (Exception $e) {
+		return 'reply no JSON format';
+	}
+	
+	$collection = getConnectionDB();
+	if (!$collection) return;
+	
+	$post = array(
+         'time'     => time(),
+		 'event'     => $jsObject['event'],
+		 'timestamp'     => $jsObject['timestamp'],
+		 'message_token'     => $jsObject['message_token'],
+         'content'   => $text
       );
 	
     $collection->insertOne($post);
 
-return;
+}
 
-
-$REQUEST_METHOD = $_SERVER['REQUEST_METHOD'];
-
-$arrayParam ='';
-
-if ($REQUEST_METHOD=='POST')
+function getConnectionDB()
 {
-	$text = implode("", file('php://input'));
 
-	$responce = file_get_contents('http://4098.ru/viber-1c', 
-	false, 
-	stream_context_create(
-		array(
-			'http' => array(
-				'method' => 'POST',
-				'header' => 'Content-Type: application/json; ',
-				'content' => $text
-				)
-			)
-		)
-	);
-    
-    echo $responce;
-}
-else
-{
-	$arrayParam = http_build_query($_GET);
-    // if (isset($_GET['q'])) 
-	// 	$queryAddr = '?q='.$_GET['q'];
-	if ($arrayParam) $arrayParam = '?'.$arrayParam;
-	$responce = file_get_contents('http://4098.ru/viber-1c'.$arrayParam);
+	$connectionString =  getenv('MONGODB_URI');
 
-	 echo $responce;
+	if (!$connectionString) $connectionString ="mongodb://heroku_7jhm3vw7:usl2213vp3pdhlaj3h0a1jo11m@ds153179.mlab.com:53179/heroku_7jhm3vw7";
+
+	$arr = array_reverse(explode('/', $connectionString));
+
+	$dbName = $arr[0];
+
+	if (!$dbName) {echo 'no db name'; return;};
+	if (!$connectionString) {echo 'no connection string'; return;};
+
+	$client = new MongoDB\Client($connectionString);
+
+	$DataBase = $client->$dbName;
+
+	$collection = $DataBase->incoming;
+	
+	return $collection;
 }
 
 
 
-function str_last_replace($search, $replace, $subject){
-    $pos = strrpos($subject, $search);
-    if($pos !== false)    {
-        $subject = substr_replace($subject, $replace, $pos, strlen($search));
-    }
-    return $subject;
-}
-
-
-//return;
-	// отправляем запрос
 
 
 ?>

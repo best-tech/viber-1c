787f723269bfd2f29e39aadb8fda070a074c57a3
Denis Kharchenko <dee@MacBook-Air-Denis.local>
2017-03-19 19:16:42 +0300

    all of

--- web/index.php
+++ web/index.php
@@ -151,4 +151,228 @@ function getConnectionDB()
 	return $collection;
 }
 
+$REQUEST_METHOD = $_SERVER['REQUEST_METHOD'];
+
+if ($REQUEST_METHOD=='POST')
+{
+	$authDate = auth();
+	$countUserQuery = getenv('COUNT_USER_QUERY');
+
+	if ($authDate['login']=='test'&&$authDate['QueryCount']>$countUserQuery) {
+		header(' 500 Internal Server Error', true, 500);
+		die("to many queries for test login ");};
+
+	if ((isset($authDate['QueryPerMonth'])&&isset($authDate['QueryMonth']))&&$authDate['QueryPerMonth']>0 && $authDate['QueryMonth']<$authDate['QueryPerMonth']) {
+		header(' 500 Internal Server Error', true, 500);
+		die("to many queries per month ");};
+
+	if (isset($_SERVER['HTTP_FILENAME']))
+	{
+		$filename = $_SERVER['HTTP_FILENAME'];	
+	}
+	elseif (isset(getallheaders()['filename'])) {
+		$filename	= getallheaders()['filename'];
+	}
+	else {
+		header(' 500 Internal Server Error', true, 500);
+		die("need to filename on headers");
+	};
+
+
+	$baseText = (string) implode("", file('php://input'));
+
+	$unicnameTime= ((string) time());
+	$unicname= substr($authDate['paid'],10).$unicnameTime.'-'.$filename;
+
+	$unicname =  substr($unicname,-180);
+
+	$responce = insertOnefile($baseText,$authDate,$filename,$unicname);
+
+	echo $unicname;
+}
+
+else
+{
+	if (isset($_GET['service'])) {
+		service();
+		echo 'service';
+		return;
+	}
+	
+	if (!isset($_GET['filename'])) die('need to &filename');
+
+	$file = getFile($_GET['filename']);
+
+	// header('Content-Type: application/x-unknown');
+	header("Content-Disposition: attachment; filename=".$file['filename']); 
+	echo base64_decode($file['content']);
+
+}
+
+function service(){
+
+	$DataBase = getConnectionDB();
+
+	$files = $DataBase->files;
+
+	$curTime = time()-1800;
+
+	$deleteFilter = ['time' =>['$lt'=>$curTime]];
+	
+	$cursor = $files->deleteMany($deleteFilter);
+	
+	
+}
+
+function getUserAccount($paid,$login,$pass){
+
+	$DataBase = getConnectionDB();
+	
+	$users = $DataBase->users;
+
+	$user = $users->findOne(['paid' => $paid]);
+
+	$tempUser = getTemplateUser();
+	// пользователь не найден
+	if (!$user){	
+		$user = $tempUser;
+		$user['paid'] 	= $paid;
+		$user['login'] 	= 'test';
+		$user['pass'] 	= 'test';
+		
+		$users->insertOne($user);
+		$tempUser = $user;
+	}
+	else
+	{
+		foreach ($tempUser as $key => $value)
+		{
+			$tempUser[$key]=$user[$key];	
+		}
+	}
+	
+	if(!$tempUser['login']==$login||!$tempUser['pass']==$pass) { die("Access forbidden");};
+
+	$tempUser['QueryCount'] = $tempUser['QueryCount']+1;
+
+	$user = $users->updateOne(['paid' => $paid],['$set' => $tempUser]);
+
+	return $tempUser;
+}
+
+function getTemplateUser()
+{
+	$arrTemp = array();
+	$arrTemp['paid'] 			='';
+	$arrTemp['QueryCount'] 		=0;
+	$arrTemp['login'] 			='';
+	$arrTemp['pass'] 			='';
+	$arrTemp['QueryPerMonth'] 	=0;
+	$arrTemp['QueryMonth'] 		=0;
+
+	return $arrTemp;
+}
+
+function auth() {
+
+	$paid = getPAID();
+
+  if (!isset($_SERVER['PHP_AUTH_USER'])||!isset($_SERVER['PHP_AUTH_PW'])) {
+	 header('WWW-Authenticate: Basic realm="viber-1c.herokuapp.com - need to login');
+     header('HTTP/1.0 401 Unauthorized');
+     echo "Вы должны ввести корректный логин и пароль для получения доступа к ресурсу \n";
+     die("Access forbidden");
+   } 
+   else {
+    
+	$login 	= $_SERVER['PHP_AUTH_USER'];
+	$pass 	= $_SERVER['PHP_AUTH_PW']; 
+
+	$UserAccount = getUserAccount($paid,$login,$pass);
+
+	return $UserAccount;
+   };
+
+}
+  
+function getFile($filename)
+{
+
+	$DataBase = getConnectionDB();
+
+	$files = $DataBase->files;
+
+	$document = $files->findOne(['unicname' =>$filename]);
+
+	if (!$document)
+	{	
+		$files = $DataBase->settings;
+		$document = $files->findOne(['key' =>"notfound"]);
+
+	}
+	
+	$arrResult['content'] = $document['content'];
+	$arrResult['filename'] = $document['filename'];
+
+	return $arrResult;
+
+}
+
+function insertOnefile($baseText,$authDate,$filename,$unicname)
+{
+	if(!$baseText) die('no incoming data');
+	
+	$lengthFile = getenv('MAX_FILE_SIZE');
+
+	if (!$lengthFile) $lengthFile = 30200;
+
+	if (strlen($baseText)>$lengthFile) die('file is too large');
+
+	$DataBase = getConnectionDB();
+	$files = $DataBase->files;
+	
+	$post = array(
+         'time'     	=> time(),
+		 'unicname'   	=> $unicname,
+		 'filename'   	=> $filename,
+		 'paid'     	=> $authDate['paid'],
+         'content'   	=> $baseText
+      );
+
+    $files->insertOne($post);
+
+}
+
+function getConnectionDB()
+{
+	$connectionString =  getenv('MONGODB_URI');
+	$arr = array_reverse(explode('/', $connectionString));
+	$dbName = $arr[0];
+
+	if (!$dbName) {die('no db name');};
+
+	if (!$connectionString) {die('no connection string');};
+	$client = new MongoDB\Client($connectionString);
+	$DataBase = $client->$dbName;
+	return $DataBase;
+}
+
+function getPAID(){
+
+	if (isset($_SERVER['HTTP_PAID']))
+	{
+		$paid = $_SERVER['HTTP_PAID'];	
+	}
+	elseif (isset(getallheaders()['paid'])) {
+		$paid	= getallheaders()['paid'];
+	}
+	else {
+		header(' 500 Internal Server Error', true, 500);
+		die("need to id pablic account");
+	};
+
+	$paid = str_replace("pa:","",$paid);
+	return $paid;
+}
+?>
 ?>
